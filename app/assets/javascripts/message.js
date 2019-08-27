59ca4f2f9b095f4f3f55a65f6c53b31af351fdc1
shimapokemon <p_nuts.mc@icloud.com>
2019-08-27 15:05:17 +0900

    非同期通信テイク２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
   function buildHTML(message){
     if ( message.image) {
-    var html =
-      `<div class="message" data-message-id=${message.id}>
+      insertImage = `<img src="${message.image}">`;
+    }
+    var html =　`<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
             ${message.user.name}
@@ -19,31 +20,30 @@ $(function(){
         <asset_path src=${message.image} >
       </div>`
     return html;
-    } else {
-    var html =
-      `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-    return html;
+    // } else {
+    // var html =
+    //   `<div class="message" data-message-id=${message.id}>
+    //       <div class="upper-message">
+    //         <div class="upper-message__user-name">
+    //           ${message.user_name}
+    //         </div>
+    //         <div class="upper-message__date">
+    //           ${message.date}
+    //         </div>
+    //       </div>
+    //       <div class="lower-message">
+    //         <p class="lower-message__content">
+    //           ${message.content}
+    //         </p>
+    //       </div>
+    //     </div>`
+    // return html;
   };
-}
+
 
   $('#new_message').on('submit', function(e){
     console.log('Unko!');
     e.preventDefault();
-    // console.log(Unko)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
