3cb45be0ff51c8b9842c6f129fcf0b82077bfc6c
shimapokemon <p_nuts.mc@icloud.com>
2019-08-27 12:31:14 +0900

    非同期通信テイク１

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,70 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image) {
+    var html =
+      `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user.name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <asset_path src=${message.image} >
+      </div>`
+    return html;
+    } else {
+    var html =
+      `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+    return html;
+  };
+}
+
+  $('#new_message').on('submit', function(e){
+    console.log('Unko!');
+    e.preventDefault();
+    // console.log(Unko)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+    })
+    .fail(function(){
+      alert('error');
+    })
+    return false;
+  })
+});
+
+
