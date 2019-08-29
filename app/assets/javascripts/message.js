7beff6fa380ca3bcd1f08b07bad0c669cf04c0f7
shimapokemon <p_nuts.mc@icloud.com>
2019-08-29 17:55:52 +0900

    自動更新
    微調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,8 +2,6 @@ $(function(){
   function buildHTML(message){
     console.log(message.image);
     var image = message.image ? `<img src=${message.image}>` : "";
-
-
     var html = 
               `<div class="message" data-message-id=${message.id}>
                   <div class="upper-message">
@@ -52,27 +50,23 @@ $(function(){
   var reloadMessages = function() {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id = $(`.message:last`).data("message-id");
-      // binding.pry
       $.ajax({
-      url: "api/messages",      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      type: 'get',      //ルーティングで設定した通りhttpメソッドをgetに指定
+      url: "api/messages",    
+      type: 'get',      
       dataType: 'json',
-      data: {last_id: last_message_id}     //dataオプションでリクエストに値を含める
+      data: {last_id: last_message_id}     
       })
     .done(function(messages) {
-      var insertHTML = '';      //追加するHTMLの入れ物を作る
+      var insertHTML = '';    
 
-      messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        console.log('キタキタ');
-        insertHTML = buildHTML(message);//メッセージが入ったHTMLを取得
-        $('.messages').append(insertHTML); //メッセージを追加
-      console.log('success');
+      messages.forEach(function (message) {
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
       })
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function() {
-      alert('自動更新に失敗しました');//ダメだったらアラートを出す
-      console.log('error');
+      alert('自動更新に失敗しました');
     });
     }
   };
