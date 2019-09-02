a2877a0f056fa581cbed0a9492a5f58c888ac141
shimapokemon <p_nuts.mc@icloud.com>
2019-09-02 13:38:11 +0900

    インクリ修正

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -20,10 +20,10 @@ function appendProduct(user) {
 
   function addUser(id,name) {
     var html = `<div class='chat-group-user'>
-               <input name='group[user_ids][]' type='hidden' value='${ id }'>
-               <p class='chat-group-user__name'>${ name }</p>
-               <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-             </div>`
+                <input name='group[user_ids][]' type='hidden' value='${ id }'>
+                <p class='chat-group-user__name'>${ name }</p>
+                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+              </div>`
     member_list.append(html);
       }
 
